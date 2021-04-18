fa70f69bedc1c43ea6d47b40ce822c332089a1fb
Piotr Domian <domianpiotr111@gmail.com>
2021-04-18 19:27:28 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function multiplyBy() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+function multiplyBy(arr, number) 
+{
+  for (i = 0; i < arr.length; i++)
+  {
+    arr[i] *= number; 
+  }
+
+  return arr;
 };
 
 module.exports = multiplyBy;
